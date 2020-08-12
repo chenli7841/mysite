b575d911ae9626c11cc702199c97fe3953f8138e
chenli888 <philosopherchenli@yahoo.com>
2020-08-12 02:19:56 -0400

    fix bug

--- jobs/net/extractBinance/RecordOrderbook.cs
+++ jobs/net/extractBinance/RecordOrderbook.cs
@@ -9,14 +9,24 @@ using Binance.Net.Objects.Spot.MarketData;
 
 namespace extractBinance
 {
+
+    public class SymbolInfo
+    {
+        public string targetCurrency;
+
+        public string baseCurrency;
+    }
+
     public class RecordOrderbook
     {
-        int? currentDay = null;
-        int lastSecond = -1;
         readonly BinanceSocketClient socketClient;
         readonly BigQueryClient bqClient;
         readonly BinanceClient binanceClient;
         readonly string symbol;
+        BinanceOrderBook lastRecord = null;
+        static readonly Dictionary<string, SymbolInfo> SYMBOL_MAP = new Dictionary<string, SymbolInfo>() {
+            { "BTCUSDT", new SymbolInfo() { baseCurrency = "USDT", targetCurrency = "BTC" }}
+        };
         public RecordOrderbook(string symbol)
         {
             if (symbol == null || symbol.Trim() == "")
@@ -31,46 +41,75 @@ namespace extractBinance
         }
         public void execute()
         {
-
-            //BinanceOrderBook data = binanceClient.GetOrderBook("BTCUSDT").Data;
-            //insertRow(data, getDateText(now));
+            setupTable(DateTime.UtcNow);
             
-            socketClient.SubscribeToOrderBookUpdates("BTCUSDT", 1000, (data) => 
+            socketClient.SubscribeToOrderBookUpdates(this.symbol, 1000, (data) => 
             {
                 DateTime now = DateTime.UtcNow;
-                if (currentDay == null || now.Day != currentDay)
-                {
-                    setupTable();
-                    currentDay = now.Day;
-                }
-                insertRow(data, getDateText(now));
+                insertRow(data);
             });
         }
 
-        private void insertRow(BinanceOrderBook data, string tableName)
+        private void insertRow(BinanceOrderBook data)
         {
-            DateTime? time = null;
-            if (data.EventTime.HasValue)
+            if (lastRecord == null)
             {
-                time = data.EventTime.Value.Second == lastSecond ?
-                    data.EventTime.Value : getTime(data.EventTime.Value);
-                lastSecond = data.EventTime.Value.Second;
+                lastRecord = data;
+                return;
             }
-            
-            BigQueryInsertRow row = new BigQueryInsertRow(insertId: time == null ? null : time.ToString())
+            DateTime currTime = data.EventTime.Value.ToUniversalTime();
+            DateTime prevTime = lastRecord.EventTime.Value.ToUniversalTime();
+            if (lastRecord.EventTime.Value > data.EventTime.Value)
             {
-                { "t", data.EventTime },
-                { "bid", BigQueryNumeric.Parse(data.Bids.ToArray()[0].Price.ToString()) },
-                { "ask", BigQueryNumeric.Parse(data.Asks.ToArray()[0].Price.ToString()) },
-                { "base", "USDT" },
-                { "target", "BTC" },
-                { "s", "BTCUSDT" },
-                { "localTime", DateTime.UtcNow }
-            };
-            bqClient.InsertRow("firebase-lispace", "binance_orderbooks", tableName, row);
+                return;
+            }
+            else if (prevTime.Second == currTime.Second)
+            {
+                lastRecord = data;
+                return;
+            }
+            else if (prevTime.Day == currTime.Day)
+            {
+                DateTime until = truncateToSecond(currTime);
+                for (DateTime currSecond = truncateToSecond(prevTime); currSecond < until; currSecond = addOneSecond(currSecond))
+                {
+                    insertToBigQuery(currSecond, lastRecord);
+                }
+                lastRecord = data;
+                return;
+            }
+            else
+            {
+                DateTime until = truncateToSecond(currTime);
+                setupTable(currTime);
+                for (DateTime currSecond = truncateToSecond(prevTime); currSecond < until; currSecond = addOneSecond(currSecond))
+                {
+                    insertToBigQuery(currSecond, lastRecord);
+                }
+                lastRecord = data;
+                return;
+            }
         }
 
-        private void setupTable()
+        private void insertToBigQuery(DateTime currSecond, BinanceOrderBook record)
+        {
+            string baseCurrency = SYMBOL_MAP[symbol].baseCurrency;
+            string targetCurrency = SYMBOL_MAP[symbol].targetCurrency;
+
+            BigQueryInsertRow row = new BigQueryInsertRow(insertId: currSecond.ToString())
+            {
+                { "t", addOneSecond(currSecond) },
+                { "bid", BigQueryNumeric.Parse(record.Bids.ToArray()[0].Price.ToString()) },
+                { "ask", BigQueryNumeric.Parse(record.Asks.ToArray()[0].Price.ToString()) },
+                { "base", baseCurrency },
+                { "target", targetCurrency },
+                { "s", symbol },
+                { "localTime", DateTime.UtcNow }
+            };
+            bqClient.InsertRow("firebase-lispace", "binance_orderbooks", getTableName(addOneSecond(currSecond)), row);
+        }
+
+        private void setupTable(DateTime dateTime)
         {
             DateTime now = DateTime.UtcNow;
             TableReference src = new TableReference()
@@ -83,19 +122,24 @@ namespace extractBinance
             {
                 ProjectId = "firebase-lispace",
                 DatasetId = "binance_orderbooks",
-                TableId = getDateText(now)
+                TableId = getTableName(dateTime)
             };
             bqClient.CreateCopyJob(src, dst);
         }
 
-        private DateTime getTime(DateTime time)
+        private DateTime truncateToSecond(DateTime time)
         {
             DateTime utc = time.ToUniversalTime();
             utc = utc.AddMilliseconds(-utc.Millisecond);
             return utc;
         }
 
-        private string getDateText(DateTime date)
+        private DateTime addOneSecond(DateTime time)
+        {
+            return time.AddSeconds(1);
+        }
+
+        private string getTableName(DateTime date)
         {
             return $"{date.Year}_{date.Month}_{date.Day}";
         }
